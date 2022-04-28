38210c1dff419fe62d1559dd10f19798e0ab8763
Lutfil Khaliq <86217611+MasonHuman@users.noreply.github.com>
2022-04-28 10:46:40 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
-
-let sonarPin = 0
+const config: [string] = [
+    "70,1200",
+    "50,1000",
+    "30,700",
+    "20,500",
+    "15,250",
+    "10,125",
+    "7,62",
+    "4,50"
+]
+let sonarPin = sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P3,
+    PingUnit.Centimeters
+)
 const buzzerPin = DigitalPin.P8
 basic.forever(function () {
-    sonarPin = sonar.ping(
-        DigitalPin.P2,
-        DigitalPin.P3,
-        PingUnit.Centimeters
-    )
     if (sonarPin == 0) {
         basic.showLeds(`
         . . # . .
@@ -25,24 +33,16 @@ basic.forever(function () {
         `)
         return
     }
-    if (sonarPin >= 70) {
-        beap(buzzerPin, 1200)
-    } else if (sonarPin >= 50) {
-        beap(buzzerPin, 1000)
-    } else if (sonarPin >= 30) {
-        beap(buzzerPin, 700)
-    } else if (sonarPin >= 20) {
-        beap(buzzerPin, 500)
-    } else if (sonarPin >= 15) {
-        beap(buzzerPin, 250)
-    } else if (sonarPin >= 10) {
-        beap(buzzerPin, 125)
-    } else if (sonarPin >= 7) {
-        beap(buzzerPin, 62)
-    } else if (sonarPin >= 4) {
-        beap(buzzerPin, 50)
-    }
-    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    config.forEach((key) => {
+        const data = key.split(",")
+        
+        const dist = parseInt(data[0])
+        const wait = parseInt(data[1])
+        if (sonarPin >= dist) {
+            beap(buzzerPin, wait)
+            return
+        }
+    })
     led.toggle(3, 1)
 })
 
